b6c8d402ac2f4e33de8d5fc7de256955483f2946
unknown <rmtestard@gmail.com>
2019-05-11 10:54:45 +0200

    showing data with hooks

--- react-movie-list/src/App.js
+++ react-movie-list/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Movies } from './components/Movies';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('/movies').then(res =>
+      res.json().then(data => {
+        setMovies(data.movies);
+      })
+    );
+  }, []);
+
+  console.log(movies);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movies movies={movies} />
     </div>
   );
 }
